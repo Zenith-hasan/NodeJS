ca4e8988eb197a9695dced77db920150ab85eacf
Zenith-hasan <abuhasan323382@gmail.com>
2024-12-16 01:04:59 +0600

    hello world

---
+++ 01-Basics/01_Intorductio.js
@@ -0,0 +1,10 @@
+// ? What is NodeJS?
+// * Node.js is a server-side runtime environment built on Google Chrome's JavaScript Engine (V8 Engine). Node.js was developed by Ryan Dahl in 2009.Node.js is a cross-platform (run on Windows, Linux, Unix, macOS, and more), open-source, back-end JavaScript runtime environment, that executes JavaScript code outside a web browser.
+
+// * Node.js is a platform built on Chrome's JavaScript runtime for easily building fast and scalable network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.
+
+// * Node.js environment is event-driven and provides non-blocking I/O, that optimizes throughput and scalability in web applications. The OpenJS Foundation, facilitated by the Linux Foundation's Collaborative Projects program, now handles the Node.js distributed development.
+
+console.log("Hello World ! I am learning Node JS")
+
+console.log("I am in Zen Mode")
